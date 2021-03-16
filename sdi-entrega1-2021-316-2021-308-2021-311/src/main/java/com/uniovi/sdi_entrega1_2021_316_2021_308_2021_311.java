49e6afd8a114f3100fe1c7203564eff542f6faf7
UO246711 <UO246711@uniovi.es>
2021-03-16 14:05:44 +0100

    Fix clase main

--- sdi-entrega1-2021-316-2021-308-2021-311/src/main/java/com/uniovi/sdi_entrega1_2021_316_2021_308_2021_311.java
+++ sdi-entrega1-2021-316-2021-308-2021-311/src/main/java/com/uniovi/sdi_entrega1_2021_316_2021_308_2021_311.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyWallapopApplication {
+public class sdi_entrega1_2021_316_2021_308_2021_311 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyWallapopApplication.class, args);
+		SpringApplication.run(sdi_entrega1_2021_316_2021_308_2021_311.class, args);
 	}
 
 }
--- sdi-entrega1-2021-316-2021-308-2021-311/src/test/java/com/uniovi/sdi_entrega1_2021_316_2021_308_2021_311Tests.java
+++ sdi-entrega1-2021-316-2021-308-2021-311/src/test/java/com/uniovi/sdi_entrega1_2021_316_2021_308_2021_311Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyWallapopApplicationTests {
+class sdi_entrega1_2021_316_2021_308_2021_311Tests {
 
 	@Test
 	void contextLoads() {
