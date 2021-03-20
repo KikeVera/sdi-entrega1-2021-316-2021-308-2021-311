2da0af75793eba7ee467580f10243d421fdcd166
UO246711 <UO246711@uniovi.es>
2021-03-20 20:03:07 +0100

    Tests apartado 3.9 terminados

--- sdi-entrega1-2021-316-2021-308-2021-311/src/test/java/com/uniovi/tests/Tests_1_ResgisterTests.java
+++ sdi-entrega1-2021-316-2021-308-2021-311/src/test/java/com/uniovi/tests/Tests_1_ResgisterTests.java
@@ -4,23 +4,46 @@ package com.uniovi.tests;
 
 
 import org.junit.*;
-
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.uniovi.entities.Sale;
+import com.uniovi.entities.User;
+import com.uniovi.repositories.UsersRepository;
+import com.uniovi.services.RolesService;
+import com.uniovi.services.SalesService;
+import com.uniovi.services.UsersService;
 import com.uniovi.tests.pageobjects.PO_HomeView;
 import com.uniovi.tests.pageobjects.PO_Properties;
 import com.uniovi.tests.pageobjects.PO_RegisterView;
 import com.uniovi.tests.pageobjects.PO_View;
+
 
 import org.junit.runners.MethodSorters;
 
 
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class Tests_1_ResgisterTests {
 	
+	@Autowired
+	private UsersService usersService;
+	@Autowired
+	private RolesService rolesService;
+	
+	@Autowired
+	private SalesService salesService;
+	
+	@Autowired
+	private UsersRepository usersRepository;
+
 	static String PathFirefox65= "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024= "C:\\Users\\Kike\\Desktop\\SDI\\geckoDriver\\geckodriver024win64.exe";
 	
@@ -38,7 +61,9 @@ public class Tests_1_ResgisterTests {
 	//Antes de cada prueba se navega al URL home de la aplicación
 	@Before
 	public void setUp() throws Exception {
+		init();
 		driver.navigate().to(URL);
+		
 	}
 	
 	//Después de cadaprueba se borran las cookies del navegador
@@ -64,8 +89,10 @@ public class Tests_1_ResgisterTests {
 		// Rellenamos el formulario.
 		PO_RegisterView.fillForm(driver, "vera.cueto@gmail.com", "Enrique", "Vera", "123456", "123456");
 		// Comprobamos los elementos de Home estando identificado como usuario
-		PO_View.checkElement(driver, "text", "Bienvenido a la tienda online");
-		PO_View.checkElement(driver, "text", "Usuario autenticado como:");
+		
+		
+		PO_View.checkKey(driver,"usuarioAutenticado.message" , PO_Properties.getSPANISH());
+		PO_View.checkKey(driver,"home.message" , PO_Properties.getSPANISH());
 		PO_View.checkElement(driver, "text", "Euros");
 
 	}
@@ -106,6 +133,105 @@ public class Tests_1_ResgisterTests {
 		// Comprobamos que nos sale un aviso de que el email esta duplicado
 		PO_RegisterView.checkKey(driver, "Error.signup.passwordConfirm.coincidence", PO_Properties.getSPANISH());
 
+	}
+	
+	public void init() {
+		usersRepository.deleteAll();
+		User user1= new User("PedroDiaz@gmail.com", "Pedro", "Díaz");
+		user1.setPassword("123456");
+		user1.setMoney(100);
+		user1.setRole(rolesService.getRoles()[0]);
+		User user2= new User("LucasNuñez@gmail.com", "Lucas", "Núñez");
+		user2.setPassword("123456");
+		user2.setMoney(100);
+		user2.setRole(rolesService.getRoles()[0]);
+		User user3= new User("MariaRodriquez@gmail.com", "María", "Rodríguez");
+		user3.setPassword("123456");
+		user3.setMoney(100);
+		user3.setRole(rolesService.getRoles()[0]);
+		User user4= new User("LuciaGonzalez@gmail.com", "Lucía", "González");
+		user4.setPassword("123456");
+		user4.setMoney(100);
+		user4.setRole(rolesService.getRoles()[0]);
+		User user5= new User("PabloVega@gmail.com", "Pablo", "Vega");
+		user5.setPassword("123456");
+		user5.setMoney(100);
+		user5.setRole(rolesService.getRoles()[0]);
+		User user6= new User("MartaSuarez@gmail.com", "Marta", "Suarez");
+		user6.setPassword("123456");
+		user6.setMoney(100);
+		user6.setRole(rolesService.getRoles()[0]);
+		User user7= new User("AntonioMoran@gmail.com", "Antonio", "Morán");
+		user7.setPassword("123456");
+		user7.setMoney(100);
+		user7.setRole(rolesService.getRoles()[0]);
+		User user8= new User("admin@email.com", "Administrador", "Administrador");
+		user8.setMoney(0);
+		user8.setPassword("admin");
+		user8.setRole(rolesService.getRoles()[1]);
+		
+		
+	
+		usersService.addUser(user1);
+		usersService.addUser(user2);
+		usersService.addUser(user3);
+		usersService.addUser(user4);
+		usersService.addUser(user5);
+		usersService.addUser(user6);
+		usersService.addUser(user7);
+		usersService.addUser(user8);
+		
+		
+		Sale sale1 = new Sale("Bici de montaña", "Bici de degunda mano", 200.50, user1);
+		Sale sale2 = new Sale("Bici de carretera", "Bici de degunda mano", 300, user2);
+		Sale sale3 = new Sale("Ordenador portatil HP", "Portatil", 400, user3);
+		Sale sale4 = new Sale("Ordenador de sobremesa", "PC viejo", 150, user4);
+		Sale sale5 = new Sale("Nevera con congelador", "Nevera", 80, user5);
+		Sale sale6 = new Sale("Ruedas de bici", "Ruedas de bici antipinchazo", 30, user6);
+		Sale sale7 = new Sale("Juego de mesa", "Juego de mesa del monoply", 150, user7);
+		Sale sale8 = new Sale("Mesa de salón", "Mesa de roble", 50.25, user1);
+		Sale sale9 = new Sale("Telescopio", "Telescopio", 200, user2);
+		Sale sale10 = new Sale("Gafas de buceo", "Gafas de buceo", 10, user3);
+		Sale sale11 = new Sale("Gafas de cerca", "Gafas para leer de cerca", 50, user4);
+		Sale sale12 = new Sale("Gafas de sol", "Gafas", 30, user5);
+		Sale sale13 = new Sale("Desbrozadora", "Desbrozadora", 95.5, user6);
+		Sale sale14 = new Sale("Barco de playmobil", "Barco pirata de playmovil", 60, user7);
+		Sale sale15 = new Sale("Barco de pesca", "Barco de pesca como nuevo", 20000, user1);
+		Sale sale16= new Sale("Gato hidraulico", "Gato hidraulico", 44.5, user2);
+		Sale sale17 = new Sale("Rascador para gatos", "Rascador", 15, user3);
+		Sale sale18 = new Sale("Castillo para gatos", "Castillo grande para gatos", 100, user4);
+		Sale sale19 = new Sale("Coche de playmobil", "Coche de policia de playmobil", 27.8, user5);
+		Sale sale20 = new Sale("Helicoptero de playmobil", "Desbrozadora", 40.5, user6);
+		Sale sale21 = new Sale("Helicoptero", "Helicoptero", 135000, user7);
+		
+		sale3.setOutstanding(true);
+		sale15.setOutstanding(true);
+		sale21.setOutstanding(true);
+
+		
+		salesService.addSale(sale1);
+		salesService.addSale(sale2);
+		salesService.addSale(sale3);
+		salesService.addSale(sale4);
+		salesService.addSale(sale5);
+		salesService.addSale(sale6);
+		salesService.addSale(sale7);
+		salesService.addSale(sale8);
+		salesService.addSale(sale9);
+		salesService.addSale(sale10);
+		salesService.addSale(sale11);
+		salesService.addSale(sale12);
+		salesService.addSale(sale13);
+		salesService.addSale(sale14);
+		salesService.addSale(sale15);
+		salesService.addSale(sale16);
+		salesService.addSale(sale17);
+		salesService.addSale(sale18);
+		salesService.addSale(sale19);
+		salesService.addSale(sale20);
+		salesService.addSale(sale21);
+			
+		
 	}
 	
 	
