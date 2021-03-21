b01915f820a2ff91c9e8aa4815f0c1c2e6188a99
UO246711 <UO246711@uniovi.es>
2021-03-21 20:31:54 +0100

    SetUp y tabla atreglados

--- sdi-entrega1-2021-316-2021-308-2021-311/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ sdi-entrega1-2021-316-2021-308-2021-311/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -27,31 +27,31 @@ public class InsertSampleDataService {
 	public void init() {
 		User user1= new User("PedroDiaz@gmail.com", "Pedro", "Díaz");
 		user1.setPassword("123456");
-		user1.setMoney(100);
+		user1.setMoney(800);
 		user1.setRole(rolesService.getRoles()[0]);
 		User user2= new User("LucasNuñez@gmail.com", "Lucas", "Núñez");
 		user2.setPassword("123456");
-		user2.setMoney(100);
+		user2.setMoney(500);
 		user2.setRole(rolesService.getRoles()[0]);
 		User user3= new User("MariaRodriquez@gmail.com", "María", "Rodríguez");
 		user3.setPassword("123456");
-		user3.setMoney(100);
+		user3.setMoney(300);
 		user3.setRole(rolesService.getRoles()[0]);
 		User user4= new User("LuciaGonzalez@gmail.com", "Lucía", "González");
 		user4.setPassword("123456");
-		user4.setMoney(100);
+		user4.setMoney(600);
 		user4.setRole(rolesService.getRoles()[0]);
 		User user5= new User("PabloVega@gmail.com", "Pablo", "Vega");
 		user5.setPassword("123456");
-		user5.setMoney(100);
+		user5.setMoney(200);
 		user5.setRole(rolesService.getRoles()[0]);
 		User user6= new User("MartaSuarez@gmail.com", "Marta", "Suarez");
 		user6.setPassword("123456");
-		user6.setMoney(100);
+		user6.setMoney(1000);
 		user6.setRole(rolesService.getRoles()[0]);
 		User user7= new User("AntonioMoran@gmail.com", "Antonio", "Morán");
 		user7.setPassword("123456");
-		user7.setMoney(100);
+		user7.setMoney(30000);
 		user7.setRole(rolesService.getRoles()[0]);
 		User user8= new User("admin@email.com", "Administrador", "Administrador");
 		user8.setMoney(0);
@@ -118,6 +118,24 @@ public class InsertSampleDataService {
 		salesService.addSale(sale19);
 		salesService.addSale(sale20);
 		salesService.addSale(sale21);
+		
+		salesService.buy(user1, sale2);
+		salesService.buy(user1, sale4);
+		salesService.buy(user2, sale1);
+		salesService.buy(user2, sale5);
+		salesService.buy(user3, sale6);
+		salesService.buy(user3, sale7);
+		salesService.buy(user4, sale9);
+		salesService.buy(user4, sale13);
+		salesService.buy(user5, sale14);
+		salesService.buy(user5, sale16);
+		salesService.buy(user6, sale3);
+		salesService.buy(user6, sale8);
+		salesService.buy(user7, sale15);
+		salesService.buy(user7, sale11);
+	
+		
+		
 		
 		
 		
